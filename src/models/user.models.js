3a65d616b502749e697477f670885f0291a18c24
gitAarya <aaryagithub@gmail.com>
2025-05-06 13:53:23 +0545

    feat: enhance User model with additional fields and methods for authentication

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,13 +1,72 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const UserSchema = new mongoose.Schema(
     {
         username:{
             type:String,
-            required:true
-        }
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true,
+        },
+        fullName:{
+            type:String,
+            required:true,
 
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+
+        },
+        avatar:{
+            type:String,
+
+        },
+        password:{
+            type:String,
+            required:[true,"password is required"]
+        },
+        refreshToken:{
+            type:String,
+            
+        }
     },
     {
         timestamps:true
     }
 )
+
+UserSchema.methods.isPasswordCorrect=async function (password) {
+    return await bcrypt.compare(password,this.password)   
+    }
+    UserSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRE
+        }
+    )
+    }
+    UserSchema.methods.generateRefreshToken=function(){
+      return jwt.sign( 
+         {  
+              _id:this._id,
+         },
+         process.env.REFRESH_TOKEN_SECRET,
+         {
+         expiresIn:process.env.REFRESH_TOKEN_EXPIRE
+         }) 
+     
+        
+    }
+    export const User= mongoose.model("User",UserSchema)
